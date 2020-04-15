b95e4737b833e883889b38903f5c9483134aef58
1268marcos <49215259+1268marcos@users.noreply.github.com>
2020-04-15 16:34:26 -0300

    Add files via upload

---
+++ ExemploListaTry.java
@@ -0,0 +1,38 @@
+package aovivo08abril2020;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ExemploListaTry {
+	
+	public static void main(String[] args) {
+		
+		int soma =0;
+		
+		//dica para quem nao tem o ECLIPSE    https://repl.it/
+		try {
+			ArrayList<Integer> numeros = new ArrayList<Integer>();
+			numeros.add(33);
+			numeros.add(3);
+			numeros.add(51);
+			numeros.add(12);
+			numeros.add(1);
+			numeros.add(0);
+			numeros.add(303);
+			Collections.sort(numeros);
+			for (int i = 0; i < numeros.size(); i++) {
+				System.out.println(  numeros.get(i) );
+				soma = soma + numeros.get(i);
+			}
+			System.out.println("A soma de valores é " + soma);
+			
+		} catch (Exception e) {
+			System.out.println("Falha \n" + e.getMessage() );
+		}
+		
+		
+	}
+	
+
+}
