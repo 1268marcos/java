0cb46cf464e2f585cc68465192592840a6b6258a
1268marcos <49215259+1268marcos@users.noreply.github.com>
2020-04-24 18:31:21 -0300

    Create conversao_tipo_1.java

---
+++ conversao_tipo_1.java
@@ -0,0 +1,28 @@
+package conversao;
+
+import java.util.Scanner;
+
+public class Exercicio {
+
+	public static void main(String args[]) {
+		
+		//implicita
+		//double a = 10.0;
+		//int b = a;
+		
+		//explicita
+		double m = 3.5;
+		int n =(int)m;
+		
+		double r = Double.parseDouble("153.11");
+		
+		int x = Integer.parseInt("2");
+		
+		double k = 55.3;
+		String texto = Double.toString(k);
+		
+		int v = 4;
+		String s = Integer.toString(v);
+	}
+	
+}
