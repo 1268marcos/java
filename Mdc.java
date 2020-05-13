a8d0d1320d6fa937343d3fc3749c9fca2a50ec8a
Marcos Santos <49215259+1268marcos@users.noreply.github.com>
2020-05-13 17:54:53 -0300

    Create Mdc.java

---
+++ Mdc.java
@@ -0,0 +1,51 @@
+package aovivo13maio2020mdc;
+
+import java.util.Scanner;
+
+public class Mdc {
+
+	public static void main(String[] args) {
+		
+		Scanner tcd = new Scanner(System.in);
+		
+		try {
+			
+			int num1=0;
+			System.out.println("Informe o primeiro número");
+			num1 = tcd.nextInt();
+			
+			int num2=0;
+			System.out.println("Informe o segundo número");
+			num2 = tcd.nextInt();
+			
+			int menor, maior;
+			
+			if( num1 > num2 ) {
+				maior = num1;
+				menor = num2;
+			}  else {
+				maior = num2;
+				menor = num1;
+			}
+			
+			//System.out.println("Maior" + maior);
+			
+			int aux = 0;
+			
+			while( (maior % menor) != 0   ) {
+				aux = menor;
+				menor = (maior % menor);
+				maior = aux;
+			}
+			
+			System.out.println("MDC ( " + num1 + " , " + num2 + " ) = " + menor);
+			
+			
+		} catch(Exception e ) {
+			System.out.println("Falha " + e.getMessage());
+		}
+		
+		
+	}
+	
+}
