173613e9f9ab5a1b6f6765502dc3ccd2ff928e0a
Marcos Santos <49215259+1268marcos@users.noreply.github.com>
2020-06-10 16:45:06 -0300

    Create CalculaDistancia1.java

---
+++ CalculaDistancia1.java
@@ -0,0 +1,49 @@
+package aovivod10junho2020alcance;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class CalculaDistancia {
+
+	public static void main(String[] args) {
+		
+		double g, anguloRadianos, d;
+		double anguloLancamentoGraus;
+		double veloc;   //    m/s
+		g = 9.8;//    
+		
+		Scanner tcd = new Scanner(System.in);
+		
+		System.out.println("Digite o angulo de lancamento");
+		anguloLancamentoGraus = tcd.nextDouble();
+		
+		System.out.println("Digite a velocidade de lancamento");
+		veloc = tcd.nextDouble();
+		
+		anguloRadianos = anguloLancamentoGraus * Math.PI / 180;
+		
+		d = 2 * veloc * veloc * 
+				Math.sin(anguloRadianos) *
+				Math.cos(anguloRadianos) / g ;
+		
+		NumberFormat nf = new DecimalFormat("#,##0.00");
+				
+		System.out.println("Distância " + nf.format( d ) + " metros" );
+		
+		for(int i = 0; i <= 180; i = i + 10) {
+			
+			anguloRadianos = i * Math.PI / 180;
+			
+			d = 2 * veloc * veloc * 
+					Math.sin(anguloRadianos) *
+					Math.cos(anguloRadianos) / g ;
+			
+			System.out.println("Angulo " + i + "º Distância " + nf.format( d ) + " metros" );
+			
+		}
+		
+			
+	}	
+	
+}
