567f0591b5972e2ecf7ef705beaf9650f48dc171
Marcos Santos <49215259+1268marcos@users.noreply.github.com>
2020-06-24 15:35:30 -0300

    Create desenhar3.java

---
+++ desenhar3.java
@@ -0,0 +1,21 @@
+package aovivo24junho2020triangulo;
+
+public class Desenhar {
+
+	public static void main(String[] args) {
+		
+		int numeroLinhas = 10;
+		
+		for( int i=1; i<=numeroLinhas; i++ ) {
+			
+			for( int j=1 ; j<=i; j++ ) {
+				System.out.print("col "+ j + " ");
+			}
+			
+			System.out.println( " lin " + i );
+			
+		}
+		
+	}
+	
+}
