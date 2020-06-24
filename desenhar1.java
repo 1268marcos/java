921dda7026abc74b3f1f930af2e0bbab62fcbd92
Marcos Santos <49215259+1268marcos@users.noreply.github.com>
2020-06-24 15:34:47 -0300

    Create desenhar1.java

---
+++ desenhar1.java
@@ -0,0 +1,21 @@
+package zaovivo24junho2020triangulo;
+
+public class ContruindoTriangulo {
+
+	public static void main(String[] args) {
+		
+		int linhas = 10;
+		
+		for(int i=1; i<=linhas; i++) {
+			
+			for(int j=1; j<=i; j++) {
+				System.out.print(" * ");
+			}
+			
+		  System.out.println("");
+			
+		}
+			
+	}
+	
+}
